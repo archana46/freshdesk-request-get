852d299870f64047feea8a0e523269d05d54d251
archana46 <archanabaskar006@gmail.com>
2018-03-14 11:13:07 +0530

    Remove useless ui things

--- app/app.js
+++ app/app.js
@@ -7,14 +7,9 @@ $(document).ready( function() {
                 client.data.get('ticket')
                     .then(function(data) {
                         console.log('data obtained:',data);
-                        $('#apptext').text("result is"+ data.ticket.subject);
-                        var l = '<ol><li>ticket subject:'+data.ticket.subject+'</li></ol>'
-                        $('#apptext').append(l);
                         client.data.get('contact')
                         .then(function(archu){
                             console.log('data loaded:',archu);
-                            var z= '<ol><li><a href= "https://ducky.freshdesk.com/a/contacts/36005037939" target="_blank">contact name: a </a>requestor email is:'+ archu.contact.email+'</li></ol>'
-                            $('#apptext').append(z);
                             var contactid= archu.contact.id;
                             var headers = {
                                 "Authorization": "Basic cHJrbGh3a2RPOUkxZGxWR3l1Zw==",
